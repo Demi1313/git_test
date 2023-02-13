5ba28139e5e6a3c64fcc0c52e7494af5ae63e11d
Dmitry <dmitrynikotin@mail.ru>
2023-02-13 11:45:56 +0300

    my first app

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-let userName = 'Dmitry';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов', ''),
+      b = prompt('оцените фильм', ''),
+      c = prompt('один из последних просмотренных фильмов', ''),
+      d = prompt('оцените фильм', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
