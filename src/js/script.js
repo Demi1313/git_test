41de457bbc04d83d6a09aac84a265e22c19c1b96
Dmitry <dmitrynikotin@mail.ru>
2023-02-27 12:32:56 +0300

    app upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,112 +1,264 @@
 "use strict";
 
-let num = 20;
 
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 10;
-    console.log(num);
-}
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel:  function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genres = prompt(`Ваш любимый жанр под номером ${i}`);
 
-showFirstMessage('hello world');
-console.log(num);
-
-function calc (a,b) {
-    return (a + b);
-}
-
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-    console.log('hello');
+            if(genres === '' || genres == null) {
+                console.log('Вы ввели некорректные данные или не ввели их совсем');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genres;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любмый жанр ${i + 1} - это ${item}`);
+        });
+    }
 };
-logger();
-
-const calc = (a, b) => a + b;
-
-// Создайте функцию, которая будет вычислять объем и площадь полной поверхности куба (тоже базовая математика, иногда используется в создании анимаций). Эта функция принимает в себя целое число со значением длины ребра куба. Ответ выведите в формате строки, который изображен в примерах.
-
-// Если в функцию попал неправильный аргумент или вычислить значения невозможно - вернуть строку "При вычислении произошла ошибка"
-function calculateVolumeAndArea(length) { //
-    if (typeof (length) !== 'number' || length < 0 ||!Number.isInteger(length)) {
-        return 'При вычислении произошла ошибка';
-    }
-    let volume = 0;
-    area = 0;
-    volume = length * length * length;
-    area = 6 * (length * length);
-    console.log(`Объём куба: ${volume}, площадь всей поверхности: ${area}`);
-    return `Объём куба: ${volume}, площадь всей поверхности: ${area}`;
-}
-calculateVolumeAndArea(5);
-
-//Напишите функцию, которая будет определять номер купе по переданному ей номеру места.
-//Функция принимает только целое число от 1 до 36.Если переданный аргумент не число, отрицательное или дробное - возвращается сообщение:"Ошибка. Проверьте правильность введенного номера места" Если число 0 или больше 36, то сообщение: "Таких мест в вагоне не существует"
-function getCoupeNumber(number) {
-    if (typeof (number) !== 'number' || number < 0 || !Number.isInteger(number)) {
-        return 'Ошибка. Проверьте правильность введенного номера места';
-    }
-    if (number > 36 || number < 1) {
-        return 'Таких мест в вагоне не существует';
-    }
-    console.log(Math.ceil(number / 4));
-    return Math.ceil(number / 4);
-}
-getCoupeNumber(5);
-
-
-//Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки. (Смотри пример). Обратите внимание на окончание слова "час" - оно меняется в зависимости от цифры. Если вместо аргумента приходит не число, дробное или отрицательное число - функция возвращает строку "Ошибка, проверьте данные"
-
-function getTimeFromMinutes(minutesTotal) {
-    if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
-        return "Ошибка, проверьте данные";
-    }
-
-    const hours = Math.floor(minutesTotal / 60);
-    const minutes = minutesTotal % 60;
-
-    let hoursStr = '';
-
-    switch (hours) {
-        case 1:
-            hoursStr = 'час';
-            break;
-        case 2:
-        case 3:
-        case 4:
-            hoursStr = 'часа';
-            break;
-        default:
-            hoursStr = 'часов';
-    }
-    console.log(`Это ${hours} ${hoursStr} и ${minutes} минут`);
-    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
-}
-
-getTimeFromMinutes(29);
 
 
 
-//Напишите функцию, которая принимает в себя 4 числа и возвращает самое большее из них. Если один из аргументов не является числом или их меньше 4 - возвращается 0. Дробные числа разрешены.
-function findMaxNumber(a, b, c, d) {
-    if (typeof(a) !== 'number' ||
-    typeof(b) !== 'number' ||
-    typeof(c) !== 'number' ||
-    typeof(d) !== 'number') {
-    return 0;
-} else {
-    console.log(Math.max(a, b ,c, d));
-    return Math.max(a, b ,c, d);
-}
-}
-findMaxNumber(1, 5, 6.6, 10.5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 5,
+//     b = a;
+// b = b + 5;
+
+// console.log(a);
+// console.log(b);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+// const copy = obj; //ссылка на объект
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y:4
+//     }
+// };
+// const newNumbers = copy(numbers);
+// newNumbers.a = 10;
+// console.log(newNumbers);
+// console.log(numbers);
+
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// console.log(Object.assign(numbers, add));
+
+
+
+// const oldArr = ['a', 'b', 'c'];
+// const newArr = oldArr.slice();
+// newArr[1] = 'bubuk';
+// console.log(oldArr);
+// console.log(newArr);
+
+
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'livejournal', 'blogger'],
+//     internet = [...video, ...blogs, 'vk', 'facebook'];
+// console.log(internet);
+
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+// log(...num);
+
+
+// const array = ['a', 'b'];
+// const newArrray = [...array];
+// console.log(array);
+// console.log(newArrray);
+
+// const arr = [1, 12, 23, 6, 8];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`)
+// });
+
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
+
+
+
+
+
+
+
+
+
+
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('test');
+//     }
+// };
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length);
+
+
+
+// console.log(options['colors']['border']);
+
+// delete options.name;
+
+// console.log(options);
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+//     for (let i = 5; i < 11; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+// }
+// fifthTask();
